164868fa0cbfd80c24123c7939c8e30247f59893
demo-ksm <khushimatlani35@gmail.com>
2026-04-23 16:49:41 +0530

    Add files via upload

---
+++ colors.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  primary: '#8B2252',
+  primaryLight: '#B5446E',
+  primarySoft: '#F5E6ED',
+  secondary: '#C4627A',
+  background: '#F8F6F4',
+  card: '#FFFFFF',
+  text: '#1C1C1E',
+  subtext: '#8A8A8E',
+  border: '#EBEBED',
+  muted: '#F2F0EE',
+  success: '#34A853',
+  successSoft: '#E8F5EC',
+  warning: '#F4A228',
+  warningSoft: '#FEF3E2',
+  info: '#4A90D9',
+  infoSoft: '#EBF3FC',
+  surface: '#FDFCFB',
+};
+
+export const fonts = {
+  regular: 'Poppins_400Regular',
+  medium: 'Poppins_500Medium',
+  semiBold: 'Poppins_600SemiBold',
+  bold: 'Poppins_700Bold',
+};
